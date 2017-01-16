c6b2634208a302f61ed72b7898f1a9e4a0a55e2a
Rberbert99 <Rberbert99@yahoo.com>
2017-01-15 19:34:10 -0700

    Ruby the hard way.

---
+++ RubyHard.rb
@@ -0,0 +1,68 @@
+#Lesson 1
+# puts "hello world"
+# puts "hello again"
+# puts 'i like typing this'
+# puts 'this is fun'
+# puts 'Yay pringitng'
+# puts 'rather you not'
+
+
+#Lesson 2
+# puts 'i will now count my chickens:'
+
+# puts "Hens #{25 + 30 / 6}"
+# puts "Roosters #{100 - 25 * 3 % 4}"
+# puts "Now i will count the eggs:"
+
+# puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# puts 3 + 2 < 5 - 7
+
+# puts "What is 3 + 2? #{3 + 2}"
+
+# puts "What is 5 - 7? #{5 - 7}"
+
+# puts 'Oh, thats why its false.'
+
+# puts 'How about some more'
+
+# puts "Is it greater? #{5 > - 2}"
+
+
+#Lesson 4
+# puts "There are #{100} cars available."
+# puts "There are only #{30} drivers available."
+# puts "There will be #{100 - 30} empty cars today"
+# puts "We can transport #{30 * 4.0} people today."
+# puts "We have #{90} peopel to carpool today"
+# puts "We need to put about #{90 / 30} in each car."
+
+
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180 
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+
+
+
+
+puts "Lets talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He weights #{my_weight} lbs."
+puts "Freaking lightweight little boy."
+puts "He has #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If i add #{my_age}, #{my_height}, and #{my_weight} . I get #{my_age + my_height + my_weight} ."
+
+
+
+
+
+
+
+
